3fb0f6015198ae45711e07ab1473a311def1cf94
yuriy.devichenskiy <yuriy.devichenskiy@shtormtech.ru>
2020-07-29 11:42:29 +0300

    IE Killing Tool

--- CloseThatFcknIE/Program.cs
+++ CloseThatFcknIE/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace CloseThatFcknIE
 {
@@ -6,7 +8,18 @@ namespace CloseThatFcknIE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ies = Process.GetProcesses().Where(x => x.ProcessName == "iexplore");
+
+            Console.WriteLine($"Total to kill: {ies.Count()}");
+
+            foreach (var proc in ies)
+            {
+                proc.Kill();
+            }
+
+            Console.WriteLine("Farewell fuckn IE!");
+            Console.WriteLine("Press any button...");
+            Console.ReadKey();
         }
     }
 }
